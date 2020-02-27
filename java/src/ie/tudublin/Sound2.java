cf5a56e5f5510a6a5b67b7ec7d204d9467508697
lema-lucau <55754825+lema-lucau@users.noreply.github.com>
2020-02-27 10:04:16 +0000

    first commit

--- java/src/ie/tudublin/Sound2.java
+++ java/src/ie/tudublin/Sound2.java
@@ -10,6 +10,8 @@ public class Sound2 extends PApplet
 
 	int frameSize = 1024;
 
+	float frameToSeconds = 44100 / (float) frameSize;
+
 	public void settings()
 	{
 		size(1024, 500);
@@ -24,6 +26,40 @@ public class Sound2 extends PApplet
 	}
 
 	float lerpedw = 0;
+
+	int countZeroCrossings()
+	{
+		int count = 0;
+
+		for(int i = 1; i < as.bufferSize(); i++)
+		{
+			if(as.left.get(i-1) > 0 && as.left.get(i) <= 0)
+			{
+				count++;
+			}
+		}
+
+		return count;
+	}
+
+	void drawLines()
+	{
+		float cx = width / 2;
+		float cy = height / 2;
+		int radius = cx;
+
+		for(int i = 0 ; i < as.bufferSize() ; i ++)
+		{
+			stroke(
+				map(i, 0, as.bufferSize(), 0, 255)
+				, 255
+				, 255
+			);
+
+			float theta = map(i, 0, as.bufferSize(), 0, TWO_PI);
+			float x = cx;
+		}
+	}
 
 	public void keyPressed()
 	{
@@ -58,6 +94,12 @@ public class Sound2 extends PApplet
 			, 255
 		);
 		ellipse(400 , cy,w, w);
-		ellipse(600 , cy,lerpedw, lerpedw);		
-}
+		ellipse(600 , cy,lerpedw, lerpedw);	
+		
+		int count = countZeroCrossings();
+
+		float freq = count * frameToSeconds;
+		textSize(22);
+		text(freq, 100, 50);
+	}
 }
